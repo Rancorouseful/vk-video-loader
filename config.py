8cd3caf067a139b3a08b95c78cf17b48e8daf34d
Rancorouseful <80785407+Rancorouseful@users.noreply.github.com>
2023-02-27 21:21:28 +0300

    Revert "vk-video-loader"
    This reverts commit cba0a1603773f4c352ac2c916d6f58163364e615.

--- config.py
+++
@@ -1,3 +0,0 @@
-TOKEN = "vk1.a.KqVxSPytagS53haV6JvMEHwSUalJzOJt0AxRnNjt9hltlIuf3GwY0qQOz4AugHrQ5y9vHIl2PmIfQretylsFmCNTeKj4PG3j7m2w9Epo8uUnCxR9dsRfZ2KKuem6w1z_2-pzT0TX_Fc-qOSZRppOmJ08FnEq5sBS9eY-yPk-XPV3OuT3foSG4nESrB1gYxSsLNwtVg_cqoB-_vR8AH6FVg"
-
-groups = ['dtp_sam', 'chp_dtp_perm', 'mosdtp',]
--- download.py
+++
@@ -1,8 +0,0 @@
-import main
-import youtube_dl
-
-ydl_opts = {}
-
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    for link in main.links:
-        ydl.download([link])
